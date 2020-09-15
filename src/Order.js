1bae775c830ecc4dbb149a0d3864ed266e617637
shakibHossain <shakib.hossain.sh@gmail.com>
2020-09-14 21:43:56 -0600

    make orders page responsive

--- src/Order.js
+++ src/Order.js
@@ -8,9 +8,9 @@ function Order({ order }) {
   return (
     <div className="order">
       <h2>Order</h2>
-      <p>{moment.unix(order.data.created).format("MMMM Do YYYY, h:mma")}</p>
-      <p className="order__id">
-        <small>{order.id}</small>
+      <p className="order__id">Order ID: {order.id}</p>
+      <p className="order__date">
+        {moment.unix(order.data.created).format("MMMM Do YYYY, h:mma")}
       </p>
 
       {order.data.basket?.map((item) => (
@@ -24,7 +24,9 @@ function Order({ order }) {
         />
       ))}
       <CurrencyFormat
-        renderText={(value) => <h3 className="order__total">Order Total: {value}</h3>}
+        renderText={(value) => (
+          <h3 className="order__total">Order Total: {value}</h3>
+        )}
         decimalScale={2}
         value={order.data.amount / 100}
         displayType={"text"}
